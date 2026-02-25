dcd30f151b4aad70fb5690f0630de0ee8aeadeb9
Macohi <sinconsistencia@gmail.com>
2026-02-24 21:17:05 -0600

    dance then center

--- source/funkin/ui/title/TitleScreen.hx
+++ source/funkin/ui/title/TitleScreen.hx
@@ -14,6 +14,7 @@ class TitleScreen extends FunkinState
 
 		titleGF = CharacterRegistry.instance.fetchCharacter('title-gf');
 		add(titleGF);
+		titleGF.dance();
 		titleGF.screenCenter();
 
 		conductor.bpm = 102;
