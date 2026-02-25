49437e28732241bfdcada8841b2a87a2aaf6ca3b
Macohi <sinconsistencia@gmail.com>
2026-02-24 21:19:19 -0600

    update conductor and center titleGF please? (and camZoom :D)

--- source/funkin/ui/title/TitleScreen.hx
+++ source/funkin/ui/title/TitleScreen.hx
@@ -7,6 +7,8 @@ import funkin.play.Character;
 class TitleScreen extends FunkinState
 {
 	var titleGF:Character;
+	
+	var camZoom:Float = 1.15;
 
 	override function create()
 	{
@@ -14,13 +16,17 @@ class TitleScreen extends FunkinState
 
 		titleGF = CharacterRegistry.instance.fetchCharacter('title-gf');
 		add(titleGF);
+
 		titleGF.dance();
+		titleGF.updateHitbox();
+		 
 		titleGF.screenCenter();
 
 		conductor.bpm = 102;
 		conductor.time = 0;
 		FlxG.sound.playMusic(Paths.sound('music/freakyMenu'), 1, true);
-		
+
+		FlxG.camera.zoom = camZoom;
 	}
 
 	override function beatHit(beat:Int)
@@ -33,5 +39,8 @@ class TitleScreen extends FunkinState
 	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		conductor.time += elapsed * Constants.MS_PER_SEC;
+		conductor.update();
 	}
 }
